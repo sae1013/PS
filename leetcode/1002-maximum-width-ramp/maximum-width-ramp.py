132e35687a8719a55f560ff8ce9f0de7f3dc6779
Minwoo Jung <sae1013@gmail.com>
2024-10-12 13:24:41 +0900

    Time: 354 ms (81.21%) | Memory: 23.7 MB (86.42%) - LeetSync

--- leetcode/1002-maximum-width-ramp/maximum-width-ramp.py
+++ leetcode/1002-maximum-width-ramp/maximum-width-ramp.py
@@ -1,15 +1,14 @@
 class Solution:
     def maxWidthRamp(self, nums: List[int]) -> int:
         # 스택에 인덱스를 내림차순으로 저장
-        stack = []
+        st = [] 
+        ans = 0
         for i in range(len(nums)):
-            if not stack or nums[stack[-1]] > nums[i]:
-                stack.append(i)
-
-        max_width = 0
-        # 배열을 뒤에서부터 순회하며 최대 너비를 갱신
-        for j in range(len(nums) - 1, -1, -1):
-            while stack and nums[stack[-1]] <= nums[j]:
-                max_width = max(max_width, j - stack.pop())
+            if not st or nums[st[-1]] > nums[i] :
+                st.append(i)
         
-        return max_width
+        for i in range(len(nums)-1,-1,-1):
+            while st and nums[st[-1]] <= nums[i]:
+                ans = max(ans, i-st[-1]) 
+                st.pop()
+        return ans
