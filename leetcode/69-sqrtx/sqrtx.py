b0a77cc28cd3974f6bffcdeb6774184a0b4b672a
Minwoo Jung <sae1013@gmail.com>
2025-01-20 16:58:18 +0900

    Time: 1 ms (70.70%) | Memory: 17.9 MB (6.39%) - LeetSync

---
+++ leetcode/69-sqrtx/sqrtx.py
@@ -0,0 +1,15 @@
+# binary search
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        ll = 0
+        rr = x
+        ans = 0
+        while ll <= rr :
+            mid = (ll+rr) // 2
+            if mid**2 > x :
+                rr = mid-1
+            else:
+                ans = mid 
+                ll = mid+1
+        return ans 
+        
